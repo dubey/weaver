fcc9ac56eea0d7c13fd35bef8800601e74b1316d
Ayush Dubey <dubey@cs.cornell.edu>
2013-11-12 15:38:50 -0500

    Graph generation algorithms using Networkx.

--- tests/create_graph.py
+++ tests/create_graph.py
@@ -15,6 +15,7 @@ import sys
 sys.path.append('../bindings/python')
 
 import client
+from test_base import test_graph
 
 # creating line graph
 nodes = []
@@ -22,14 +23,15 @@ num_nodes = 100
 print client._CLIENT_ID
 c = client.Client(client._CLIENT_ID+1, 0)
 print 'Created client'
-for i in range(num_nodes):
-    tx_id = c.begin_tx()
-    nodes.append(c.create_node(tx_id))
-    c.end_tx(tx_id)
-    print 'Created node ' + str(i)
-for i in range(num_nodes-1):
-    tx_id = c.begin_tx()
-    c.create_edge(tx_id, nodes[i], nodes[i+1])
-    c.end_tx(tx_id)
-    print 'Created edge ' + str(i)
+tg = test_graph(c, num_nodes, 2*num_nodes)
+#for i in range(num_nodes):
+#    tx_id = c.begin_tx()
+#    nodes.append(c.create_node(tx_id))
+#    c.end_tx(tx_id)
+#    print 'Created node ' + str(i)
+#for i in range(num_nodes-1):
+#    tx_id = c.begin_tx()
+#    c.create_edge(tx_id, nodes[i], nodes[i+1])
+#    c.end_tx(tx_id)
+#    print 'Created edge ' + str(i)
 print 'Created graph'
