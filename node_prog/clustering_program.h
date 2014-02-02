0531f7cb4c9fb84cebaea05b019f563fea7b29fe
Greg Hill <gdh39@cornell.edu>
2014-02-02 16:28:07 -0500

    draft for clustering

--- node_prog/clustering_program.h
+++ node_prog/clustering_program.h
@@ -135,102 +135,6 @@ namespace node_prog
             message::unpack_buffer(unpacker, numerator);
         }
     };
-    /*
-
-    inline void
-    add_to_cache(uint64_t numerator, common::node &n, common::node_ptr &rn, vc::vclock &req_vclock,
-            std::function<void(std::shared_ptr<node_prog::Cache_Value_Base>,
-                std::shared_ptr<std::vector<common::node_ptr>>, uint64_t)>& add_cache_func)
-    {
-        std::shared_ptr<node_prog::clustering_cache_value> toCache(new clustering_cache_value(numerator));
-        std::shared_ptr<std::vector<common::node_ptr>> watch_set(new std::vector<common::node_ptr>());
-        // add center node and valid neighbors to watch set XXX re-use old list for re-caching
-        watch_set->emplace_back(rn);
-        for (std::pair<const uint64_t, common::edge*> &possible_nbr : n.out_edges) {
-            if (order::clock_creat_before_del_after(req_vclock, possible_nbr.second->get_creat_time(), possible_nbr.second->get_del_time()))
-            {
-                watch_set->emplace_back(possible_nbr.second->nbr);
-            }
-        }
-        uint64_t cache_key = rn.handle;
-        add_cache_func(toCache, watch_set, cache_key);
-    }
-
-    inline void
-    calculate_response(clustering_node_state &cstate, common::node &n, common::node_ptr &rn, vc::vclock &req_vclock,
-            std::vector<std::pair<common::node_ptr, clustering_params>> &next,
-            clustering_params &params, std::function<void(std::shared_ptr<node_prog::Cache_Value_Base>,
-                std::shared_ptr<std::vector<common::node_ptr>>, uint64_t)>& add_cache_func)
-    {
-        if (cstate.neighbor_counts.size() > 1) {
-            double denominator = (double) (cstate.neighbor_counts.size() * (cstate.neighbor_counts.size() - 1));
-            uint64_t numerator = 0;
-            for (std::pair<const uint64_t, int> nbr_count : cstate.neighbor_counts){
-                numerator += nbr_count.second;
-            }
-            params.clustering_coeff = (double) numerator / denominator;
-            if (MAX_CACHE_ENTRIES)
-            {
-                add_to_cache(numerator, n, rn, req_vclock, add_cache_func);
-            }
-        } else {
-            params.clustering_coeff = 0;
-        }
-        common::node_ptr coord(params.vt_id, 1337);
-        next.emplace_back(std::make_pair(common::node_ptr(params.vt_id, 1337), params));
-    }
-
-    inline bool
-    check_cache_context(std::vector<std::pair<common::node_ptr, db::caching::node_cache_context>>& context, common::node_ptr& center)
-    {
-        // if center unchanged and no neighbor was deleted cache is valid
-        for (auto& pair : context)
-        {
-            if (pair.first == center){
-                assert(!pair.second.node_deleted);
-                if (!pair.second.edges_added.empty() || !pair.second.edges_deleted.empty()){
-                    return false;
-                }
-            } else if (pair.second.node_deleted){
-                    return false;
-            }
-        }
-        return true;
-    }
-
-    inline double
-    calculate_from_cached(std::vector<std::pair<common::node_ptr, db::caching::node_cache_context>>& context,
-            std::vector<common::node_ptr>& watch_set, common::node_ptr& center, uint64_t numerator,
-            vc::vclock &req_vclock, common::node &n, common::node_ptr &rn,
-            std::function<void(std::shared_ptr<node_prog::Cache_Value_Base>,
-                std::shared_ptr<std::vector<common::node_ptr>>, uint64_t)>& add_cache_func)
-    {
-        if (watch_set.size() <= 2){
-            return 0;
-        }
-        double denominator = (watch_set.size()-1)*(watch_set.size()-2);
-
-        for (auto& pair : context)
-        {
-            assert(!pair.second.node_deleted);
-            if (pair.first == center){
-                continue;
-            }
-            for (auto& e : pair.second.edges_added){
-                if (e.nbr != center && std::find(watch_set.begin(), watch_set.end(), e.nbr) != watch_set.end()){
-                    numerator++;
-                }
-            }
-            for (auto& e : pair.second.edges_deleted){
-                if (e.nbr != center && std::find(watch_set.begin(), watch_set.end(), e.nbr) != watch_set.end()){
-                    numerator--;
-                }
-            }
-        }
-        add_to_cache(numerator, n, rn, req_vclock, add_cache_func);
-        return (double) numerator / denominator;
-    }
-    */
 
     std::vector<std::pair<common::node_ptr, clustering_params>> 
     clustering_node_program(
@@ -242,73 +146,49 @@ namespace node_prog
                 std::shared_ptr<std::vector<common::node_ptr>>, uint64_t)>& add_cache_func,
             std::unique_ptr<db::caching::cache_response> cache_response)
     {
-        std::vector<std::pair<common::node_ptr, clustering_params>> next;
-        /*
-        if (MAX_CACHE_ENTRIES)
-        {
-        if (params._search_cache && cache_response != NULL){
-            // check context, update cache
-            bool valid = check_cache_context(cache_response->context, rn);
-            if (valid) {
-                //WDEBUG  << "WEEE GOT A valid CACHE RESPONSE, short circuit" << std::endl;
-                std::shared_ptr<clustering_cache_value> val = std::dynamic_pointer_cast<clustering_cache_value>(cache_response->value);
-                params.clustering_coeff = calculate_from_cached(cache_response->context, *cache_response->watch_set,
-                        rn, val->numerator, *req_vclock, n, rn, add_cache_func);
-                common::node_ptr coord(params.vt_id, 1337);
-                next.emplace_back(std::make_pair(coord, params));
-
-                return next;
-            }
-            cache_response->invalidate();
-        }
-        params._search_cache = false; // only search cache for first of req
-        }
         if (params.is_center) {
             node_prog::clustering_node_state &cstate = state_getter();
             if (params.outgoing) {
-                    params.is_center = false;
-                    params.center = rn;
-                    common::edge *e;
-                    for (std::pair<const uint64_t, common::edge*> &possible_nbr : n.out_edges) {
-                        e = possible_nbr.second;
-                        if (order::clock_creat_before_del_after(*req_vclock, e->get_creat_time(), e->get_del_time()))
-                        {
-                            next.emplace_back(std::make_pair(possible_nbr.second->nbr, params));
-                            cstate.neighbor_counts.insert(std::make_pair(possible_nbr.second->nbr.handle, 0));
-                            cstate.responses_left++;
-                            e->traverse();
-                        }
-                    }
-                    if (cstate.responses_left == 0) {
-                        calculate_response(cstate, n, rn, *req_vclock, next, params, add_cache_func);
-                    }
+                std::vector<std::pair<common::node_ptr, clustering_params>> neighbor_programs;
+                params.is_center = false;
+                params.center = rn;
+                for (common::edge& edge : n.get_edges()) {
+                    neighbor_programs.emplace_back(std::make_pair(edge.get_neighbor(), params));
+                    cstate.neighbor_counts.insert(std::make_pair(edge.get_neighbor().get_handle(), 0));
+                    cstate.responses_left++;
+                }
+                if (cstate.responses_left < 2) { // if no or one neighbor we know clustering coeff already
+                    params.clustering_coeff = 0;
+                    return {std::make_pair(common::coordinator, std::move(params))};
+                } else {
+                    return neighbor_programs;
+                }
             } else {
-                for (uint64_t poss_nbr : params.neighbors) {
+                for (uint64_t nbr : params.neighbors) {
                     if (cstate.neighbor_counts.count(poss_nbr) > 0) {
                         cstate.neighbor_counts[poss_nbr]++;
                     }
                 }
-                if (--cstate.responses_left == 0){
-                    calculate_response(cstate, n, rn, *req_vclock, next, params, add_cache_func);
+                if (--cstate.responses_left == 0) {
+                    assert(cstate.neighbor_counts.size() > 1);
+                    double denominator = (double) (cstate.neighbor_counts.size() * (cstate.neighbor_counts.size() - 1));
+                    uint64_t numerator = 0;
+                    for (std::pair<const uint64_t, int>& nbr_count : cstate.neighbor_counts){
+                        numerator += nbr_count.second;
+                    }
+                    params.clustering_coeff = (double) numerator / denominator;
+                    return {std::make_pair(common::coordinator, std::move(params))};
                 }
             }
         } else { // not center
-            for (std::pair<const uint64_t, common::edge*> &possible_nbr : n.out_edges) {
-                if (order::clock_creat_before_del_after(
-                            *req_vclock, possible_nbr.second->get_creat_time(), possible_nbr.second->get_del_time()))
-                {
-                    params.neighbors.push_back(possible_nbr.second->nbr.handle);
-                }
+            for (common::edge& edge : n.get_edges()) {
+                params.neighbors.push_back(edge.get_neighbor().get_handle());
             }
             params.outgoing = false;
             params.is_center = true;
-            next.emplace_back(std::make_pair(params.center, params));
+            return {std::make_pair(params.center, std::move(params))};
         }
-        */
-        return next;
     }
-
 }
-
 
 #endif
