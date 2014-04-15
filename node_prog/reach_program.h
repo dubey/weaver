1ae2a3de7f625fc0e07d5bdc1078ad22f2566174
Greg Hill <gdh39@cornell.edu>
2014-04-14 23:07:45 -0400

    switched reach program to a split cc and h file

--- node_prog/reach_program.h
+++ node_prog/reach_program.h
@@ -174,40 +174,7 @@ namespace node_prog
         }
     };
 
-    inline bool
-    check_cache_context(cache_response<reach_cache_value> &cr)
-    {
-        std::vector<node_cache_context>& contexts = cr.get_context();
-        if (contexts.size() == 0) {
-            return true;
-        }
-        reach_cache_value &cv = *cr.get_value();
-        // path not valid if broken by:
-        for (node_cache_context& node_context : contexts)
-        {
-            if (node_context.node_deleted){  // node deletion
-                WDEBUG  << "Cache entry invalid because of node deletion" << std::endl;
-                return false;
-            }
-            // edge deletion, see if path was broken
-            for (size_t i = 1; i < cv.path.size(); i++) {
-                if (node_context.node == cv.path.at(i)) {
-                    db::element::remote_node &path_next_node = cv.path.at(i-1);
-                    for(auto &edge : node_context.edges_deleted){
-                        if (edge.nbr == path_next_node) {
-                            WDEBUG  << "Cache entry invalid because of edge deletion" << std::endl;
-                            return false;
-                        }
-                    }
-                    break; // path not broken here, move on
-                }
-            }
-        }
-        WDEBUG  << "Cache entry with context size " << contexts.size() << " valid" << std::endl;
-        return true;
-    }
-
-    inline std::vector<std::pair<db::element::remote_node, reach_params>> 
+    std::vector<std::pair<db::element::remote_node, reach_params>> 
     reach_node_program(
             node &n,
             db::element::remote_node &rn,
@@ -215,102 +182,7 @@ namespace node_prog
             std::function<reach_node_state&()> state_getter,
             std::function<void(std::shared_ptr<reach_cache_value>, // TODO make const
                 std::shared_ptr<std::vector<db::element::remote_node>>, uint64_t)>& add_cache_func,
-            cache_response<reach_cache_value>*cache_response)
-    {
-        reach_node_state &state = state_getter();
-        std::vector<std::pair<db::element::remote_node, reach_params>> next;
-        bool false_reply = false;
-        db::element::remote_node prev_node = params.prev_node;
-        params.prev_node = rn;
-        if (!params.returning) { // request mode
-            if (params.dest == rn.get_id()) {
-                // we found the node we are looking for, prepare a reply
-                params.returning = true;
-                params.reachable = true;
-                params.path.emplace_back(rn);
-                return {std::make_pair(prev_node, params)};
-            } else {
-                // have not found it yet so follow all out edges
-                if (!state.visited) {
-                    state.prev_node = prev_node;
-                    state.visited = true;
-
-                    if (MAX_CACHE_ENTRIES)
-                    {
-                        if (params._search_cache /*&& !params.returning */ && cache_response != NULL){
-                            // check context, update cache
-                            bool valid = check_cache_context(*cache_response);
-                            if (valid) {
-                                // we found the node we are looking for, prepare a reply
-                                params.returning = true;
-                                params.reachable = true;
-                                params._search_cache = false; // don't search on way back
-
-                                // context for cached value contains the nodes in the path to the dest_idination from this node
-                                params.path = std::dynamic_pointer_cast<reach_cache_value>(cache_response->get_value())->path; // XXX double check this path
-                                WDEBUG  << "Cache worked at node " << rn.id << " with path len " << params.path.size() << std::endl;
-                                /*
-                                for (auto& node_context : cache_response->get_context()) { 
-                                    params.path.emplace_back(node_context.node); // XXX THEse can be shuffled, change to storing this in cache
-                                }
-                                */
-                                return {std::make_pair(prev_node, params)}; // single length vector
-                            } else {
-                                cache_response->invalidate();
-                            }
-                        }
-                    }
-
-                    for (edge &e: n.get_edges()) {
-                        // checking edge properties
-                        if (e.has_all_properties(params.edge_props)) {
-                            e.traverse();
-                            // propagate reachability request
-                            next.emplace_back(std::make_pair(e.get_neighbor(), params));
-                            state.out_count++;
-                        }
-                    }
-                    if (state.out_count == 0) {
-                        false_reply = true;
-                    }
-                } else {
-                    false_reply = true;
-                }
-            }
-            if (false_reply) {
-                params.returning = true;
-                params.reachable = false;
-                next.emplace_back(std::make_pair(prev_node, params));
-            }
-        } else { // reply mode
-            if (params.reachable) {
-                if (state.hops > params.hops) {
-                    state.hops = params.hops;
-                }
-            }
-            if (((--state.out_count == 0) || params.reachable) && !state.reachable) {
-                state.reachable |= params.reachable;
-                if (params.reachable) {
-                    params.hops = state.hops + 1;
-                    params.path.emplace_back(rn);
-                    if (MAX_CACHE_ENTRIES)
-                    {
-                        // now add to cache
-                        WDEBUG << "adding to cache on way back from dest on node " << rn.id << " with path len " << params.path.size() << std::endl;
-                        std::shared_ptr<node_prog::reach_cache_value> toCache(new reach_cache_value(params.path));
-                        std::shared_ptr<std::vector<db::element::remote_node>> watch_set(new std::vector<db::element::remote_node>(params.path)); // copy return path from params
-                        add_cache_func(toCache, watch_set, params.dest);
-                    }
-                }
-                next.emplace_back(std::make_pair(state.prev_node, params));
-            }
-            if ((int)state.out_count < 0) {
-                WDEBUG << "ALERT! Bad state value in reach program" << std::endl;
-                next.clear();
-            }
-        }
-        return next;
-    }
+            cache_response<reach_cache_value>*cache_response);
 }
 
 #endif
