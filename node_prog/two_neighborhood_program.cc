043d076566ca6088cc4e832e069a466becec9f4d
Greg Hill <gdh39@cornell.edu>
2014-04-21 22:00:06 -0400

    first draft of prog

---
+++ node_prog/two_neighborhood_program.cc
@@ -0,0 +1,46 @@
+/*
+ * ===============================================================
+ *    Description:  Node program to read properties of a single node
+ *
+ *        Created:  Friday 17 January 2014 11:00:03  EDT
+ *
+ *         Author:  Ayush Dubey, Greg Hill
+ *                  dubey@cs.cornell.edu, gdh39@cornell.edu
+ *
+ * Copyright (C) 2013, Cornell University, see the LICENSE file
+ *                     for licensing agreement
+ * ================================================================
+ */
+
+#ifndef weaver_node_prog_two_neighborhood_program_cc 
+#define weaver_node_prog_two_neighborhood_program_cc 
+
+#include "two_neighborhood_program.h"
+
+namespace node_prog
+{
+    inline std::pair<search_type, std::vector<std::pair<db::element::remote_node, two_neighborhood_params>>>
+    two_neighborhood_node_program(
+            node &n,
+            db::element::remote_node &,
+            two_neighborhood_params &params,
+            std::function<two_neighborhood_state&()>,
+            std::function<void(std::shared_ptr<node_prog::Cache_Value_Base>,
+                std::shared_ptr<std::vector<db::element::remote_node>>, uint64_t)>&,
+            cache_response<Cache_Value_Base>*)
+    {
+        /*
+        for (property &prop : n.get_properties()) {
+            if (params.keys.empty() || (std::find(params.keys.begin(), params.keys.end(), prop.get_key()) != params.keys.end())) {
+                params.node_props.emplace_back(prop.get_key(), prop.get_value());
+            }
+        }
+
+        std::vector<std::pair<db::element::remote_node, two_neighborhood_params>> toRet;
+        toRet.emplace_back(std::make_pair(db::element::coordinator, std::move(params)));
+        return std::make_pair(search_type::DEPTH_FIRST, toRet); 
+    }
+    */
+}
+
+#endif
---
+++ node_prog/two_neighborhood_program.h
@@ -0,0 +1,123 @@
+/*
+ * ===============================================================
+ *    Description:  Node program to read properties of a single node
+ *
+ *        Created:  Friday 17 January 2014 11:00:03  EDT
+ *
+ *         Author:  Ayush Dubey, Greg Hill
+ *                  dubey@cs.cornell.edu, gdh39@cornell.edu
+ *
+ * Copyright (C) 2013, Cornell University, see the LICENSE file
+ *                     for licensing agreement
+ * ================================================================
+ */
+
+#ifndef weaver_node_prog_two_neighborhood_program_h 
+#define weaver_node_prog_two_neighborhood_program_h 
+
+#include <vector>
+#include <string>
+
+#include "common/message.h"
+#include "common/vclock.h"
+#include "common/event_order.h"
+#include "node.h"
+#include "edge.h"
+#include "db/element/remote_node.h"
+
+namespace node_prog
+{
+    class two_neighborhood_params : public virtual Node_Parameters_Base 
+    {
+        public:
+            std::string prop_key_val;
+            int hops;
+            bool outgoing;
+            db::element::remote_node prev_node;
+            std::vector<std::pair<uint64_t, std::string>> response_list;
+
+        public:
+            virtual bool search_cache() {
+                return false; // would never need to cache
+            }
+
+            virtual uint64_t cache_key() {
+                return 0;
+            }
+
+            virtual uint64_t size() const 
+            {
+                uint64_t toRet = message::size(prop_key_val)
+                    + message::size(hops)
+                    + message::size(outgoing)
+                    + message::size(prev_node)
+                    + message::size(response_list)
+                return toRet;
+            }
+
+            virtual void pack(e::buffer::packer& packer) const 
+            {
+                message::pack_buffer(packer, prop_key_val);
+                message::pack_buffer(packer, hops);
+                message::pack_buffer(packer, outgoing);
+                message::pack_buffer(packer, prev_node);
+                message::pack_buffer(packer, response_list);
+            }
+
+            virtual void unpack(e::unpacker& unpacker)
+            {
+                message::unpack_buffer(unpacker, prop_key_val);
+                message::unpack_buffer(unpacker, hops);
+                message::unpack_buffer(unpacker, outgoing);
+                message::unpack_buffer(unpacker, prev_node);
+                message::unpack_buffer(unpacker, response_list);
+            }
+    };
+
+    struct two_neighborhood_state : public virtual Node_State_Base
+    {
+        bool visited;
+        uint32_t responses_left;
+        db::element::remote_node prev_node;
+        std::vector<std::pair<uint64_t, std::string>> response_list;
+
+        virtual ~two_neighborhood_state() { }
+
+        virtual uint64_t size() const
+        {
+            uint64_t toRet = message::size(visited)
+                + message::size(responses_left)
+                + message::size(prev_node)
+                + message::size(response_list)
+                return toRet;
+        }
+
+        virtual void pack(e::buffer::packer& packer) const 
+        {
+            message::pack_buffer(packer, visited);
+            message::pack_buffer(packer, responses_left);
+            message::pack_buffer(packer, prev_node);
+            message::pack_buffer(packer, response_list);
+        }
+
+        virtual void unpack(e::unpacker& unpacker)
+        {
+            message::unpack_buffer(unpacker, visited);
+            message::unpack_buffer(unpacker, responses_left);
+            message::unpack_buffer(unpacker, prev_node);
+            message::unpack_buffer(unpacker, response_list);
+        }
+    };
+
+    inline std::pair<search_type, std::vector<std::pair<db::element::remote_node, two_neighborhood_params>>>
+    two_neighborhood_node_program(
+            node &n,
+            db::element::remote_node &,
+            two_neighborhood_params &params,
+            std::function<two_neighborhood_state&()>,
+            std::function<void(std::shared_ptr<node_prog::Cache_Value_Base>,
+                std::shared_ptr<std::vector<db::element::remote_node>>, uint64_t)>&,
+            cache_response<Cache_Value_Base>*);
+}
+
+#endif
