d149da4b09287e3082382678313680a69fdb96e0
Ayush Dubey <dubey@cs.cornell.edu>
2014-07-17 13:24:22 -0400

    Better start script, pending: list of replicas for kronos and server manager.

--- startup_scripts/start_weaver_support.sh
+++ startup_scripts/start_weaver_support.sh
@@ -23,28 +23,28 @@ kronos_ipaddr=$(weaver-parse-config -c kronos_ipaddr $config_file_args)
 kronos_port=$(weaver-parse-config -c kronos_port $config_file_args)
 
 # hyperdex
-mkdir -p hyperdex_start/coord
-cd hyperdex_start
-
-echo "Starting HyperDex coordinator at location $hyperdex_coord_ipaddr : $hyperdex_coord_port"
-cd coord
-rm -f *.log *.sst *.old CURRENT  LOCK  LOG  MANIFEST* replicant-daemon-*
-hyperdex coordinator -l $hyperdex_coord_ipaddr -p $hyperdex_coord_port > /dev/null 2>&1
+echo "Starting HyperDex coordinator at location $hyperdex_coord_ipaddr : $hyperdex_coord_port, data at ~/weaver_runtime/hyperdex/coord"
+ssh $hyperdex_coord_ipaddr "\
+    setopt nullglob && \
+    mkdir -p weaver_runtime/hyperdex/coord && \
+    cd weaver_runtime/hyperdex/coord && \
+    rm -f *.log *.sst *.old CURRENT  LOCK  LOG  MANIFEST* replicant-daemon-* && \
+    hyperdex coordinator -l $hyperdex_coord_ipaddr -p $hyperdex_coord_port > /dev/null 2>&1"
 sleep 2
 
-cd ..
 num_daemons=${#hyperdex_daemons_ipaddr[*]}
 for i in $(seq 1 $num_daemons);
 do
     idx=$(($i-1))
-    echo "Starting HyperDex daemon $i at location ${hyperdex_daemons_ipaddr[$idx]} : ${hyperdex_daemons_port[$idx]}"
-    mkdir -p daemon$idx
-    cd daemon$idx
-    rm -f *.log *.sst CURRENT  LOCK  LOG  MANIFEST-000002 hyperdex-daemon-*
-    hyperdex daemon --listen=${hyperdex_daemons_ipaddr[$idx]} --listen-port=${hyperdex_daemons_port[$idx]} \
-                    --coordinator=$hyperdex_coord_ipaddr --coordinator-port=$hyperdex_coord_port \
-                    > /dev/null 2>&1
-    cd ..
+    echo "Starting HyperDex daemon $i at location ${hyperdex_daemons_ipaddr[$idx]} : ${hyperdex_daemons_port[$idx]}, data at ~/weaver_runtime/hyperdex/daemon$idx"
+    ssh ${hyperdex_daemons_ipaddr[$idx]} "\
+        setopt nullglob && \
+        mkdir -p weaver_runtime/hyperdex/daemon$idx && \
+        cd weaver_runtime/hyperdex/daemon$idx && \
+        rm -f *.log *.sst CURRENT  LOCK  LOG  MANIFEST-000002 hyperdex-daemon-* && \
+        hyperdex daemon --listen=${hyperdex_daemons_ipaddr[$idx]} --listen-port=${hyperdex_daemons_port[$idx]} \
+                        --coordinator=$hyperdex_coord_ipaddr --coordinator-port=$hyperdex_coord_port \
+                        > /dev/null 2>&1"
 done
 
 sleep 1
@@ -112,36 +112,28 @@ EOF
 
 
 # server manager
-cd ..
-mkdir -p server_manager_start/daemon1
-mkdir -p server_manager_start/client
-
-echo "Starting server manager at location $server_manager_ipaddr : $server_manager_port"
-cd server_manager_start
-cd daemon1
-rm -f *.log *.sst *.old CURRENT  LOCK  LOG  MANIFEST* replicant-daemon-*
-replicant daemon --daemon --listen $server_manager_ipaddr --listen-port $server_manager_port > /dev/null 2>&1
+echo "Starting server manager at location $server_manager_ipaddr : $server_manager_port, data at ~/weaver_runtime/server_manager/daemon1"
+ssh $server_manager_ipaddr "\
+    setopt nullglob && \
+    mkdir -p weaver_runtime/server_manager/daemon1 && \
+    cd weaver_runtime/server_manager/daemon1 && \
+    rm -f *.log *.sst *.old CURRENT  LOCK  LOG  MANIFEST* replicant-daemon-* && \
+    replicant daemon --daemon --listen $server_manager_ipaddr --listen-port $server_manager_port > /dev/null 2>&1"
 sleep 1
 
-cd ../client
 replicant new-object -h $server_manager_ipaddr -p $server_manager_port weaver /usr/local/lib/libweaverservermanager.so
 
 
 # kronos
-cd ../..
-mkdir -p kronos_start/daemon1
-mkdir -p kronos_start/client
-
-echo "Starting Kronos at location $kronos_ipaddr : $kronos_port"
-cd kronos_start
-cd daemon1
-rm -f *.log *.sst *.old CURRENT  LOCK  LOG  MANIFEST* replicant-daemon-*
-replicant daemon --daemon --listen $kronos_ipaddr --listen-port $kronos_port > /dev/null 2>&1
+echo "Starting Kronos at location $kronos_ipaddr : $kronos_port, data at ~/weaver_runtime/kronos/daemon1"
+ssh $kronos_ipaddr "\
+    setopt nullglob && \
+    mkdir -p weaver_runtime/kronos/daemon1 && \
+    cd weaver_runtime/kronos/daemon1 && \
+    rm -f *.log *.sst *.old CURRENT  LOCK  LOG  MANIFEST* replicant-daemon-* && \
+    replicant daemon --daemon --listen $kronos_ipaddr --listen-port $kronos_port > /dev/null 2>&1"
 sleep 1
 
-cd ../client
 replicant new-object -h $kronos_ipaddr -p $kronos_port chronosd /usr/local/lib/libchronosd.so
-
-cd ../../
 
 echo 'Done startup.'
