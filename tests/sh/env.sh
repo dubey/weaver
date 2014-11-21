3d4d7213e2176bd28cf834a263da6f4a809899ed
Ayush Dubey <dubey@cs.cornell.edu>
2014-11-21 11:51:19 -0500

    add env.sh

---
+++ tests/sh/env.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+#
+# env.sh
+# Copyright (C) 2014 Ayush Dubey <dubey@cs.cornell.edu>
+#
+# See the LICENSE file for licensing agreement
+#
+
+export WEAVER_SRCDIR="$1"
+export WEAVER_BUILDDIR="$2"
