e3dc0f9ae8480b9e2e4eb21298579d888f4ea020
Ayush Dubey <dubey@cs.cornell.edu>
2014-12-02 11:14:45 -0500

    startup scripts for docker container

---
+++ startup_scripts/docker_kill_weaver.sh
@@ -0,0 +1,53 @@
+#! /bin/bash
+#
+# kill_weaver.sh
+# Copyright (C) 2014 Ayush Dubey <dubey@cs.cornell.edu>
+#
+
+# get config file location
+if [ $# -eq 1 ]
+then
+    config_file_args="-f $1"
+else
+    config_file_args=""
+fi
+
+# get config params
+hyperdex_coord_ipaddr=$(weaver-parse-config -c hyperdex_coord_ipaddr $config_file_args)
+hyperdex_coord_port=$(weaver-parse-config -c hyperdex_coord_port $config_file_args)
+hyperdex_daemons_ipaddr=($(weaver-parse-config -c hyperdex_daemons_ipaddr $config_file_args))
+hyperdex_daemons_port=($(weaver-parse-config -c hyperdex_daemons_port $config_file_args))
+server_manager_ipaddr=($(weaver-parse-config -c server_manager_ipaddr $config_file_args))
+server_manager_port=($(weaver-parse-config -c server_manager_port $config_file_args))
+kronos_ipaddr=($(weaver-parse-config -c kronos_ipaddr $config_file_args))
+kronos_port=($(weaver-parse-config -c kronos_port $config_file_args))
+
+echo "Killing all replicant daemons at $hyperdex_coord_ipaddr"
+pidof replicant-daemon | xargs kill -9
+
+num_daemons=${#hyperdex_daemons_ipaddr[*]}
+for i in $(seq 1 $num_daemons);
+do
+    idx=$(($i-1))
+    ipaddr=${hyperdex_daemons_ipaddr[$idx]}
+    echo "Killing all HyperDex daemons at $ipaddr"
+    pidof hyperdex-daemon | xargs kill -9
+done
+
+num_sm_daemons=${#server_manager_ipaddr[*]}
+for i in $(seq 1 $num_sm_daemons);
+do
+    idx=$(($i-1))
+    ipaddr=${server_manager_ipaddr[$idx]}
+    echo "Killing all replicant daemons at $ipaddr"
+    pidof replicant-daemon | xargs kill -9
+done
+
+num_kronos_daemons=${#kronos_ipaddr[*]}
+for i in $(seq 1 $num_kronos_daemons);
+do
+    idx=$(($i-1))
+    ipaddr=${kronos_ipaddr[$idx]}
+    echo "Killing all replicant daemons at $ipaddr"
+    pidof replicant-daemon | xargs kill -9
+done
---
+++ startup_scripts/docker_start_weaver.sh
@@ -0,0 +1,153 @@
+#! /bin/bash
+#
+# start_weaver.sh
+# Copyright (C) 2014 Ayush Dubey <dubey@cs.cornell.edu>
+#
+
+# get config file location
+if [ $# -eq 1 ]
+then
+    config_file_args="-f $1"
+else
+    config_file_args=""
+fi
+
+if [ -z "$WEAVER_BUILDDIR" ]
+then
+    if [ -e /usr/local/lib/libweaverservermanager.so ]
+    then
+        weaver_libdir=/usr/local/lib
+    else
+        if [ -e /usr/lib/libweaverservermanager.so ]
+        then
+            weaver_libdir=/usr/lib
+        else
+            echo 'Did not find libweaverservermanager.so at /usr/lib and /usr/local/lib.  Also, WEAVER_BUILDDIR not set.  Exiting now.'
+            exit 1
+        fi
+    fi
+else
+    weaver_libdir="$WEAVER_BUILDDIR"/.libs
+fi
+
+echo 'weaver libdir:'
+echo $weaver_libdir
+
+
+# get config params
+hyperdex_coord_ipaddr=$(weaver-parse-config -c hyperdex_coord_ipaddr $config_file_args)
+hyperdex_coord_port=$(weaver-parse-config -c hyperdex_coord_port $config_file_args)
+hyperdex_daemons_ipaddr=($(weaver-parse-config -c hyperdex_daemons_ipaddr $config_file_args))
+hyperdex_daemons_port=($(weaver-parse-config -c hyperdex_daemons_port $config_file_args))
+server_manager_ipaddr=($(weaver-parse-config -c server_manager_ipaddr $config_file_args))
+server_manager_port=($(weaver-parse-config -c server_manager_port $config_file_args))
+kronos_ipaddr=($(weaver-parse-config -c kronos_ipaddr $config_file_args))
+kronos_port=($(weaver-parse-config -c kronos_port $config_file_args))
+
+rm_patterns="*.log *.sst *.old CURRENT  LOCK  LOG  MANIFEST*"
+
+shopt -s nullglob
+cd
+
+# hyperdex
+hyperdex_coord_dir="~/weaver_runtime/hyperdex/coord"
+echo "Starting HyperDex coordinator at location $hyperdex_coord_ipaddr : $hyperdex_coord_port, data at $hyperdex_coord_dir"
+mkdir -p $hyperdex_coord_dir
+cd $hyperdex_coord_dir
+rm -f $rm_patterns replicant-daemon-*
+hyperdex coordinator -l $hyperdex_coord_ipaddr -p $hyperdex_coord_port > /dev/null 2>&1
+sleep 2
+
+num_daemons=${#hyperdex_daemons_ipaddr[*]}
+for i in $(seq 1 $num_daemons);
+do
+    idx=$(($i-1))
+    ipaddr=${hyperdex_daemons_ipaddr[$idx]}
+    port=${hyperdex_daemons_port[$idx]}
+    directory="~/weaver_runtime/hyperdex/daemon$idx"
+    echo "Starting HyperDex daemon $i at location $ipaddr : $port, data at $directory"
+    mkdir -p $directory
+    cd $directory
+    rm -f $rm_patterns hyperdex-daemon-*
+    hyperdex daemon --listen=$ipaddr --listen-port=$port \
+                    --coordinator=$hyperdex_coord_ipaddr --coordinator-port=$hyperdex_coord_port \
+                    > /dev/null 2>&1
+done
+
+sleep 1
+
+echo 'Adding HyperDex spaces'
+
+hyperdex add-space -h $hyperdex_coord_ipaddr -p $hyperdex_coord_port << EOF
+space weaver_loc_mapping
+key node
+attributes
+    int shard
+subspace shard
+create 8 partitions
+tolerate 2 failures
+EOF
+
+hyperdex add-space -h $hyperdex_coord_ipaddr -p $hyperdex_coord_port << EOF
+space weaver_graph_data
+key node
+attributes
+    string creat_time,
+    map(string, string) properties,
+    map(string, string) out_edges,
+    int migr_status,
+    string last_upd_clk,
+    string restore_clk
+tolerate 2 failures
+EOF
+
+hyperdex add-space -h $hyperdex_coord_ipaddr -p $hyperdex_coord_port << EOF
+space weaver_tx_data
+key tx_id
+attributes
+    int vt_id,
+    string tx_data
+tolerate 2 failures
+EOF
+
+
+# server manager
+num_sm_daemons=${#server_manager_ipaddr[*]}
+for i in $(seq 1 $num_sm_daemons);
+do
+    idx=$(($i-1))
+    ipaddr=${server_manager_ipaddr[$idx]}
+    port=${server_manager_port[$idx]}
+    directory="~/weaver_runtime/server_manager/daemon$idx"
+    echo "Starting server manager at location  $ipaddr: $port, data at $directory"
+    shopt -s nullglob
+    mkdir -p $directory
+    cd $directory
+    rm -f *.log *.sst *.old CURRENT  LOCK  LOG  MANIFEST* replicant-daemon-*
+    replicant daemon --daemon --listen $ipaddr --listen-port $port > /dev/null 2>&1
+done
+sleep 1
+
+replicant new-object -h ${server_manager_ipaddr[0]} -p ${server_manager_port[0]} weaver "$weaver_libdir"/libweaverservermanager.so
+
+
+# kronos
+num_kronos_daemons=${#kronos_ipaddr[*]}
+for i in $(seq 1 $num_kronos_daemons);
+do
+    idx=$(($i-1))
+    ipaddr=${kronos_ipaddr[$idx]}
+    port=${kronos_port[$idx]}
+    directory="~/weaver_runtime/kronos/daemon$idx"
+    echo "Starting Kronos at location  $ipaddr: $port, data at $directory"
+    shopt -s nullglob
+    mkdir -p $directory
+    cd $directory
+    rm -f *.log *.sst *.old CURRENT  LOCK  LOG  MANIFEST* replicant-daemon-*
+    replicant daemon --daemon --listen $ipaddr --listen-port $port > /dev/null 2>&1
+done
+sleep 1
+
+replicant new-object -h ${kronos_ipaddr[0]} -p ${kronos_port[0]} chronosd "$weaver_libdir"/libweaverchronosd.so
+
+echo 'Done startup.'
