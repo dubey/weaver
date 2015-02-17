d83bf12ec8976e93acaa1e253115b5f4321bd745
Ayush Dubey <dubey@cs.cornell.edu>
2015-02-17 16:05:21 -0500

    test node alias in node prog, bugfix in simple_test_aux_index.py.

--- tests/python/correctness/simple_test_aux_index.py
+++ tests/python/correctness/simple_test_aux_index.py
@@ -30,19 +30,19 @@ c = client.Client('127.0.0.1', 2002, config_file)
 # check aux index
 assert c.aux_index()
 
-# create node for user ayush
+# 1. create node for user ayush
 c.begin_tx()
 c.create_node('ayush')
 c.set_node_property('type', 'user', 'ayush')
 assert c.end_tx(), 'create node failed'
 
-# create node for user egs
+# 2. create node for user egs
 c.begin_tx()
 c.create_node('egs')
 c.set_node_property('type', 'user', 'egs')
 assert c.end_tx(), 'create node failed'
 
-# ayush follows egs
+# 3. ayush follows egs
 c.begin_tx()
 c.create_edge('ayush', 'egs', 'e1')
 c.set_edge_property(edge='e1', key='type', value='follows')
@@ -50,7 +50,7 @@ c.create_edge('egs', 'ayush', 'e2')
 c.set_edge_property(edge='e2', key='type', value='followed_by')
 assert c.end_tx(), 'tx fail, something is wrong'
 
-# add a post and restrict visibility to followers only
+# 4. add a post and restrict visibility to followers only
 c.begin_tx()
 c.create_node('post')
 c.set_node_property('type', 'post', 'post')
@@ -59,32 +59,38 @@ e3 = c.create_edge('egs', 'post')
 c.set_edge_property(edge=e3, key='type', value='posted')
 assert c.end_tx(), 'tx fail, something is wrong'
 
-# 'like' the post
+# 5. 'like' the post
 c.begin_tx()
 e4 = c.create_edge('post', 'ayush')
 c.set_edge_property(edge=e4, key='type', value='liked_by')
 assert c.end_tx(), 'create edge failed'
 
-# list all the people who like egs's post
+# 6. list all the people who like egs's post
 return_nodes = c.traverse('egs', {'type': 'user'}).out_edge({'type': 'posted'}).node({'type': 'post'}).out_edge({'type': 'liked_by'}).node({'type': 'user'}).execute()
 assert len(return_nodes) == 1, 'traversal returned incorrect #nodes'
 assert 'ayush' in return_nodes, 'traversal returned bad node handle'
 
-# try to create node with same handle as before
+# 7. try to create node with same handle as before
 c.begin_tx()
 c.create_node('ayush')
 assert not c.end_tx(), 'create node passed'
 
-# try to create edge with same handle as before
+# 8. try to create edge with same handle as before
 c.begin_tx()
 c.create_edge('ayush', 'egs', 'e1')
 assert not c.end_tx(), 'create edge passed'
 
-# add auxiliary handles to nodes
+# 9. add auxiliary handles to nodes
 c.begin_tx()
 c.add_alias('ad688', 'ayush')
-c.add_alias('el33th4x04', 'egs')
+c.add_alias('el33th4x0r', 'egs')
 assert c.end_tx(), 'add aux handles'
+
+# 10. list all the people who like egs's post
+# this time with aliases instead of handles
+return_nodes = c.traverse('el33th4x0r', {'type': 'user'}).out_edge({'type': 'posted'}).node({'type': 'post'}).out_edge({'type': 'liked_by'}).node({'type': 'user'}).execute()
+assert len(return_nodes) == 1, 'traversal returned incorrect #nodes'
+assert 'ayush' in return_nodes, 'traversal returned bad node handle'
 
 print 'Correctly executed 9 transactions of varying complexity, pass simple_test.'
 print 'Success, you have a working Weaver setup!'
