2c66579e164323228bd718a5ac559301ca60f5da
Ayush Dubey <dubey@cs.cornell.edu>
2015-03-31 11:31:25 -0400

    remove db/nop_data.h and add db/types.h which defines node/edge map type

---
+++ db/types.h
@@ -0,0 +1,27 @@
+/*
+ * ===============================================================
+ *    Description:  Shard data types.
+ *
+ *         Author:  Ayush Dubey, dubey@cs.cornell.edu
+ *
+ * Copyright (C) 2014, Cornell University, see the LICENSE file
+ *                     for licensing agreement
+ * ===============================================================
+ */
+
+#include <google/sparse_hash_map>
+#include <google/sparse_hash_set>
+
+#include "common/utils.h"
+
+namespace db
+{
+    template<class V> using data_map = google::sparse_hash_map<std::string,
+                                                               V,
+                                                               weaver_util::murmur_hasher<std::string>,
+                                                               weaver_util::eqstr>;
+
+    using string_set = google::sparse_hash_set<std::string,
+                                               weaver_util::murmur_hasher<std::string>,
+                                               weaver_util::eqstr>;
+}
