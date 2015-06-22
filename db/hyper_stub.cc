500c5cc1676c0bbd2c71f34cdd95106c9896fb8f
Ayush Dubey <dubey@cs.cornell.edu>
2015-06-22 11:51:47 -0400

    do not save edge ids at shards

--- db/hyper_stub.cc
+++ db/hyper_stub.cc
@@ -220,7 +220,12 @@ hyper_stub :: recover_node(db::node &n)
 {
     std::unordered_map<node_handle_t, db::node*> nodes;
     nodes.emplace(n.get_handle(), &n);
-    return get_nodes(nodes, false);
+    if (get_nodes(nodes, false)) {
+        n.edge_ids.clear();
+        return true;
+    } else {
+        return false;
+    }
 }
 
 bool
